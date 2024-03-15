913dd62b07930ee4a5e11d7a2a171d1ce39e88ae
Grzegorz Kmita <grzegorzkmita5@gmail.com>
2024-03-15 15:29:30 +0100

    New interactive map

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
 const SanbudNav = document.getElementById("SanbudNav");
 const SanbudNavBrand = document.getElementById("SanbudNavBrand");
 
